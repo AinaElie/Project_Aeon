04bc90a7275d99e2976338e4f5efac2970a50645
AinaElie <elie.anjarafanomezana@gmail.com>
2025-04-11 15:21:08 +0300

    feat: style navbar

--- app_aeon/app/components/NavBar.tsx
+++ app_aeon/app/components/NavBar.tsx
@@ -1,14 +1,20 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function NavBar() {
   return (
-    <nav>
-      <div className="w-5 text-main">
-        <FontAwesomeIcon icon={faBars} />
+    <nav className="w-full h-16 border-b border-main shadow-lg shadow-slate-500 flex flex-row justify-between items-center px-8 fixed z-50 bg-gradient-to-br from-white">
+      <div className="p-3 cursor-pointer hover:bg-gray-100 rounded-xl">
+        <div className="w-5 text-main">
+          <FontAwesomeIcon icon={faBars} />
+        </div>
       </div>
-      <div></div>
-      <div></div>
+      <div className="h-full object-top flex justify-center items-center">
+        <img src="/LogoTransparent.png" className="w-full h-32 object-cover" />
+      </div>
+      <div className="w-8 text-main cursor-pointer">
+        <FontAwesomeIcon icon={faUserCircle} />
+      </div>
     </nav>
-  )
+  );
 }
