70d58ffa151677d6f2decf8eaace216e75e22659
AinaElie <elie.anjarafanomezana@gmail.com>
2025-04-11 15:20:31 +0300

    feat: add link to dashboard

--- app_aeon/app/page.tsx
+++ app_aeon/app/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
+      <Link href={"/dashboard"} className="text-blue-950 mt-10">
+        Home
+      </Link>
     </>
   );
 }
