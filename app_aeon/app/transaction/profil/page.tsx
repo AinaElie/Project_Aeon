1757fc108948c04437a48ba5c2df884f4f57773a
AinaElie <elie.anjarafanomezana@gmail.com>
2025-04-15 10:14:29 +0300

    feat: get data user by id or profil user

---
+++ app_aeon/app/transaction/profil/page.tsx
@@ -0,0 +1,9 @@
+import MyProfil from "@/app/components/transaction/MyProfil";
+
+export default function page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <MyProfil />
+    </div>
+  )
+}
