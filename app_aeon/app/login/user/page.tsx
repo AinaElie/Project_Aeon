9b43b3fc1d838078fda593ca801918d352276fa7
AinaElie <elie.anjarafanomezana@gmail.com>
2025-06-04 12:37:03 +0300

    feat: style authentification

--- app_aeon/app/login/user/page.tsx
+++ app_aeon/app/login/user/page.tsx
@@ -1,7 +1,22 @@
-"use client"
+"use client";
 
-import { signIn } from "next-auth/react"
+import { signIn } from "next-auth/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function page() {
-  return  <button onClick={() => signIn("github")}>github</button>
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="shadow-md shadow-main rounded-lg w-1/3 flex flex-col justify-center items-center">
+        <h1 className="text-main text-3xl py-8">Connexion</h1>
+        <button
+          onClick={() => signIn("github", { redirectTo: "/transaction" })}
+          className="border border-black shadow-lg shadow-slate-500 rounded-lg hover:scale-105 w-4/5 flex justify-center items-center py-3 mb-4"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2x" className="pr-5" />
+          Github
+        </button>
+      </div>
+    </div>
+  );
 }
