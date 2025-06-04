147dba73a5a515fbf1143a0a6930bccace244caa
AinaElie <elie.anjarafanomezana@gmail.com>
2025-06-04 10:57:42 +0300

    fix: authentification github

--- transaction/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import ErrorMessage from "../components/transaction/Error"
-
-export default function Error({message} : {message: string}) {
-  return (
-    <ErrorMessage message={message} />
-  )
-}
--- transaction/profil/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProfilUser from "@/app/components/transaction/profil/ProfilUser";
-
-export default function page() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-        <ProfilUser />
-    </div>
-  )
-}
